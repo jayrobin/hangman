2b1c081bb4036978e78e05a11e6601cc5adb75a6
James Robinson <james.michael.robinson@googlemail.com>
2014-05-21 16:25:34 +0100

    add valid word check to dictionary

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -6,14 +6,22 @@ class Dictionary
 
 	def load_dictionary(dictionary_file, min_word_len=0, max_word_len=99)
 	  file = File.open(dictionary_file, 'r')
+	  contents = file.read
+
+	  @words = contents.split(/[\r\n]+/)
 
 	  # load words into array and strip short/long words as required
-	  @words = file.readlines.select { |word| word.size.between?(min_word_len, max_word_len) }
-	  
+	  @words = @words.select { |word| word.size.between?(min_word_len, max_word_len) }
+	  @words = @words.map(&:downcase)
+
 	  file.close
 	end
 
 	def get_random_word
 		@words.sample
+	end
+
+	def is_word_valid(word)
+		@words.include?(word)
 	end
 end
