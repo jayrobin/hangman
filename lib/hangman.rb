a15819af9f25e353acd614358800d02ae112e50a
James Robinson <james.michael.robinson@googlemail.com>
2014-05-21 16:41:23 +0100

    add printing intro to hangman

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,7 +2,24 @@ require_relative 'dictionary'
 
 class Hangman
 	def initialize
+	  load_dictionary
+	end
+
+	def new_game
+	  print_intro
+	end
+
+	private
+
+	def load_dictionary
 	  @dictionary = Dictionary.new
 	  @dictionary.load("dictionary.txt")
 	end
+
+	def print_intro
+		puts "Welcome to Hangman"
+		puts "James Robinson 2014"
+	end
 end
+
+Hangman.new
