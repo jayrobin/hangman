3c591c296fecdb12573e419213e793e42aef0314
James Robinson <james.michael.robinson@googlemail.com>
2014-05-21 16:14:29 +0100

    add min max word length to dictionary

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,13 +1,15 @@
 class Dictionary
-	def initialize(dictionary_file)
-		load_dictionary(dictionary_file)
+
+	def initialize
+		@words = []
 	end
 
-	private
-
-	def load_dictionary(dictionary_file)
+	def load_dictionary(dictionary_file, min_word_len=0, max_word_len=99)
 	  file = File.open(dictionary_file, 'r')
-	  @words = file.readlines
+
+	  # load words into array and strip short/long words as required
+	  @words = file.readlines.select { |word| word.size.between?(min_word_len, max_word_len) }
+	  
 	  file.close
 	end
 end
