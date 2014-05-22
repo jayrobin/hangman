d3055a5ef80b13ae3a065756fffe2b75945a03ab
James Robinson <james.michael.robinson@googlemail.com>
2014-05-22 13:22:47 +0100

    add AI player guessing letter

--- lib/ai_player.rb
+++ lib/ai_player.rb
@@ -6,9 +6,15 @@ class AIPlayer < Player
 
 	  # AI requires access to dictionary as both hangman and guesser
 	  @dictionary = dictionary
+
+	  @letters = ('a'..'z').to_a
 	end
 
 	def get_word
 		@dictionary.get_random_word
+	end
+
+	def get_guess
+		@letters.delete_at(rand(@letters.size))
 	end
 end
